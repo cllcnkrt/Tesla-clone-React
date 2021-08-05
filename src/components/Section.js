268a141af6f291525cf403f58df92ef40ad3f183
cllcnkrt <cllcnkrt@outlook.com>
2021-08-05 18:18:39 +0300

    home component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({title,description,leftBtnText,rightBtnText,backgroundImg}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
+          <LeftButton>{leftBtnText}</LeftButton>
 
-          <RightButton>Existing Inventory</RightButton>
+          <RightButton>{rightBtnText}</RightButton>
         </ButtonGroup>
 
         <DownArrow src="/images/down-arrow.svg" />
@@ -29,7 +29,7 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("/images/model-s.jpg");
+  background-image: url("/images/${(props) =>props.bgImage}");
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -44,6 +44,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width: 768px){
+      flex-direction:column;
+  }
 `;
 
 const LeftButton = styled.div`
@@ -62,13 +65,17 @@ const LeftButton = styled.div`
   margin: 8px;
 `;
 
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+background:white;
+opacity:0.65;
+color:black;
+`;
 
 const DownArrow = styled.img`
-cursor: pointer;
-  margin-top: 20px;
+  cursor: pointer;
   height: 40px;
   animation: animateDown infinite 1.5s;
+  overflow-x:hidden;
 `;
 
 const Buttons = styled.div``;
