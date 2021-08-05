dc640b757d1f2d9de291d2cebf3e76e3ae3aaf05
cllcnkrt <cllcnkrt@outlook.com>
2021-08-05 17:29:45 +0300

    animation added index css

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,12 +8,15 @@ function Section() {
         <h1>Model S</h1>
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
 
-      <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
 
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 }
@@ -45,17 +48,27 @@ const ButtonGroup = styled.div`
 
 const LeftButton = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
-  height:40px;
-  width:256px;
-  color:white;
-  display:flex;
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
   justify-content: center;
-  align-items:center;
-  border-radius:100px;
-  opacity:0.85;
-  text-transform:uppercase;
-  font-sizce:12px;
-  cursor:pointer;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-sizce: 12px;
+  cursor: pointer;
+  margin: 8px;
 `;
 
 const RightButton = styled(LeftButton)``;
+
+const DownArrow = styled.img`
+cursor: pointer;
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div``;
